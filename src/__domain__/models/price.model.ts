8b26b37b9b485826124901baa5dc9eb316cddf9f
Maciej Guzek <maciej@tipser.com>
2020-04-13 19:05:45 +0200

    One more missed file

--- src/__domain__/models/price.model.ts
+++ src/__domain__/models/price.model.ts
@@ -1,7 +1,11 @@
 import { BaseModel } from './base.model';
+import {prop} from "@typegoose/typegoose";
 
 export class PriceModel extends BaseModel<PriceModel> {
+
+    @prop()
     currency: string;
 
+    @prop()
     amount: number;
 }
